14232b6930a119961577ed735a3374508f6061ce
Hammed Nureni <kunlex.hm@gmail.com>
2025-08-04 17:56:37 -0500

    modify ansible

--- main.tf
+++ main.tf
@@ -41,6 +41,21 @@ resource "azurerm_public_ip" "public_ip" {
   sku                 = "Standard"
 }
 
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "AllowSSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+
 resource "azurerm_network_interface" "nic" {
   name                = "${var.vm_name}-nic"
   location            = azurerm_resource_group.rg.location
